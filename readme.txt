9e9f042ad86f4cd0ed35b5a9eb18649b39383899
johnnytee <johnnytee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-21 01:35:02 +0000

    Tagging 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-notification-bar/tags/1.0.0@546772 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Plugin Name ===
+Contributors: johnnytee, seedprod
+Donate link: http://seedprod.com/
+Tags: message, floating bar, notice, notification, sticky header, special offer, discount offer, offer, important, notification bar, attention bar, highlight bar
+Requires at least: 3
+Tested up to: 3.3
+Stable tag: 1.0.0
+
+A quick and easy notification bar and call to action for your site.
+
+== Description ==
+
+A quick and easy **notification bar** and **call to action** for your site.
+
+
+### Benefits, Features and Options:
+
+*   Quick & easy setup
+*   Custom message and call to action button
+*	Sticky Notification Bar
+*	Choose any color to match your site
+*   i18n Support
+*   Multisite Support
+
+
+== Screenshots ==
+
+1. Front End
+2. Back End
+
+
+== Changelog ==
+= 1.0.0 =
+* FInitial Release
+
+
+== Upgrade Notice ==
+= 1.0.0 =
+* Initial Release
---
+++ wordpress-notification-bar.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: WordPress Notification Bar
+Plugin URI: http://seedprod.com/wordpress-notification-bar/
+Description: WordPress menu and setting api wrapper for plugins.
+Version:  1.0.0
+Author: SeedProd
+Author URI: http://www.seedprod.com
+License: GPLv2
+Copyright 2012  John Turner (email : john@seedprod.com, twitter : @johnturner)
+*/
+
+/**
+ * Init
+ *
+ * @package WordPress
+ * @subpackage seed_wnb
+ * @since 0.1.0
+ */
+
+/**
+ * Default Constants
+ */
+define( 'SEED_WNB_SHORTNAME', 'seed_wnb' ); // Used to reference namespace functions.
+define( 'SEED_WNB_FILE', 'wordpress-notification-bar/wordpress-notification-bar.php' ); // Used for settings link.
+define( 'SEED_WNB_TEXTDOMAIN', 'seed_wnb' ); // Your textdomain
+define( 'SEED_WNB_PLUGIN_NAME', __( 'WordPress Notification Bar', 'seed_wnb' ) ); // Plugin Name shows up on the admin settings screen.
+define( 'SEED_WNB_VERSION', '1.0.0' ); // Plugin Version Number. Recommend you use Semantic Versioning http://semver.org/
+define( 'SEED_WNB_REQUIRED_WP_VERSION', '3.0' ); // Required Version of WordPress
+define( 'SEED_WNB_PLUGIN_PATH', plugin_dir_path( __FILE__ ) ); // Example output: /Applications/MAMP/htdocs/wordpress/wp-content/plugins/seed_wnb/
+define( 'SEED_WNB_PLUGIN_URL', plugin_dir_url( __FILE__ ) ); // Example output: http://localhost:8888/wordpress/wp-content/plugins/seed_wnb/
+
+/**
+ * Upon activation of the plugin, see if we are running the required version and deploy theme in defined.
+ *
+ * @since 0.1.0
+ */
+function seed_wnb_activation( )
+{
+    if ( version_compare( get_bloginfo( 'version' ), SEED_WNB_REQUIRED_WP_VERSION, '<' ) ) {
+        deactivate_plugins( __FILE__ );
+        wp_die( sprintf( __( "WordPress %s and higher required. The plugin has now disabled itself. On a side note why are you running an old version :( Upgrade!", 'seed_wnb' ), SEED_WNB_REQUIRED_WP_VERSION ) );
+    }
+}
+register_activation_hook( __FILE__, 'seed_wnb_activation' );
+
+/**
+ * Load Required Files
+ */
+require_once( 'framework/framework.php' );
+require_once( 'inc/config.php' );
+require_once( 'inc/class-plugin.php' );
+
+//var_dump( $seed_wnb->get_options() );
