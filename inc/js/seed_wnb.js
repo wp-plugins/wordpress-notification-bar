111fcddd20fd74022b1db07bc5c4bf1de382ae0f
johnnytee <johnnytee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-05 15:17:54 +0000

    tagging 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-notification-bar/tags/1.1.1@621277 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/js/seed_wnb.js
+++ inc/js/seed_wnb.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($){
 	if(seed_wnb_js_localize.button_label != '' && seed_wnb_js_localize.button_link != ''){
 		var button = '<a href="'+seed_wnb_js_localize.button_link+'" target="'+seed_wnb_js_localize.button_target+'"><span class="wnb-bar-button">'+seed_wnb_js_localize.button_label+'</span></a>';
 	}
-	$('body').prepend('<div id="wnb-bar"><span>' + seed_wnb_js_localize.msg + '<span> ' + button + '</div>');
+	$('body').prepend('<div id="wnb-bar"><span>' + seed_wnb_js_localize.msg + '</span> ' + button + '</div>');
 	var pt = $('body').css('padding-top');
 
 	if($('#wpwrap').length < 1){
--- wordpress-notification-bar.php
+++ wordpress-notification-bar.php
@@ -3,7 +3,7 @@
 Plugin Name: WordPress Notification Bar
 Plugin URI: http://seedprod.com/wordpress-notification-bar/
 Description: Global Notification Bar for WordPress
-Version:  1.1.0
+Version:  1.1.1
 Author: SeedProd
 Author URI: http://www.seedprod.com
 License: GPLv2
